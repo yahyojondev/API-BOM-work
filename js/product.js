9e0a0cee672656ba86fe7b64283d6a86faa0fdfc
yahyojondev <nabijonovdeveloper@gmail.com>
2024-03-13 12:38:51 +0500

    hello

--- js/product.js
+++ js/product.js
@@ -1,4 +1,22 @@
 
+const API_URL = "https://api.escuelajs.co/api/v1/products"
+const img = document.querySelector(".product_image")
+
+
 async function fetchSingleUser(api){
-    let get
+    let params = new URLSearchParams(window.location.search)
+    let id = params.get("id")
+    console.log(id);
+
+    let data = await fetch(`${api}/${id}`)
+    data 
+    .json()
+    .then(res=>singleUser(res))
+    .catch(res=>console.log(res))
+}
+fetchSingleUser(API_URL)
+
+function singleUser({image}){
+    img.setAttribute("src",img)
+    img.src= image
 }
